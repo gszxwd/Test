6aa4f64dd86f48683a5cf6789997986087e77f8e
zhaoxu <zhaoxu.work@hotmail.com>
2016-06-06 15:05:36 +0800

    re-implenment an initial method for K-means

--- color.py
+++ color.py
@@ -3,8 +3,8 @@ __author__ = 'Xu'
 import cv2
 import numpy as np
 
-# segment the background
 filename = '1/1_229.jpg'
+# segment the background
 img = cv2.imread(filename)
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 val, thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
@@ -45,7 +45,6 @@ def get_yslices(bins):
     return ret
 
 # k-means segmentation
-k = 7
 yslices = get_yslices(np.array(ybins)[:,0])
 samples = []
 for i in range(len(yslices)):
@@ -60,16 +59,50 @@ def distance(v1, v2, i=0, j=0):
 # use the matrix to store the cluster info, first column save belongings, second column save distance
 clusters = np.mat(np.ones((len(samples),2)))*-1
 
-# step1: init centroids (TODO: improve, e.g. use filtered color info)
-# method 1 (filtered color info, also determine the k value!)
+# step1: init centroids (TODO: improve, e.g. use filtered color info, but which color?)
+# method 1 (use symbol width info)
+width_min = 8
+width_max = 24
+longbins = []
+shortbins = []
+interval = 3
+# handle short bin case
+for i in range(len(yslices)):
+    if (yslices[i][1] - yslices[i][0]) < width_min:
+        shortbins.append(i)
+for i in shortbins:
+    if (i-1)>0 and (yslices[i][0]-yslices[i-1][1]) < interval:
+        yslices[i-1][1] = yslices[i][1]
+        yslices.remove(yslices[i])
+
+# handle long bin case
+for i in range(len(yslices)):
+    if (yslices[i][1] - yslices[i][0]) > width_max:
+        longbins.append(i)
+for i in longbins:
+    a = yslices[i][0]
+    b = yslices[i][1]
+    yslices.remove(yslices[i])
+    d = (b-a)/width_max
+    for j in range(d+1):
+        yslices.append([round((j/float(d+1))*(b-a)+a), round(((j+1)/float(d+1))*(b-a)+a)])
+
+# use the center of each bin as centroids
+k = len(yslices)
+centroids = []
+for i in range(k):
+    centroids.append(int((yslices[i][0]+yslices[i][1])/2))
+centroids = np.array(centroids)
 
 # method 2 (using percentile number)
+# k = 7
 # centroids = []
 # for i in range(k):
 #     centroids.append(np.int(np.percentile(samples, (100)*(i+1)/(k+1))))
 # centroids = np.array(centroids)
 
 # method 3 (using distance segmentation result)
+# k = 7
 # centroids = []
 # if len(yslices) >= k:
 #     for i in range(k):
@@ -101,13 +134,62 @@ while clusterChanged:
         pointsInCluster = np.array(samples)[tmp]
         centroids[j] = np.int(np.mean(pointsInCluster))
 
-# get the new segment
+# get the new segmented slices
 seg = clusters[:,0].tolist()
 j = 0
+flag = True
+start = end = 0
+yslices_new = []
 for i in range(len(seg)):
-    if seg[i][0] == j:
-        print i,samples[i]
+    if (seg[i][0] == j) and (not flag):
+        flag = True
+        end = samples[i-1]
+        yslices_new.append([start, end])
+    if (seg[i][0] == j) and flag:
+        flag = False
+        start = samples[i]
+        #print i, start
         j = j+1
+    if i == len(seg)-1:
+        end = samples[i]
+        yslices_new.append([start, end])
+
+# get the segmented images
+def get_xslices(xbins):
+    x = y = -1
+    for i in range(len(xbins)):
+        if xbins[i] > 0:
+            x = i-1
+            break
+    for i in range(len(xbins)-1, -1, -1):
+        if xbins[i] > 0:
+            y = i+1
+            break
+    return [x,y]
+
+def get_seg_images(img, yslices):
+    imgs = []
+    # region [x1, x2, y1, y2] list saved for further processing
+    regions = []
+    for i in range(len(yslices)):
+        img2 = img[:,yslices[i][0]:yslices[i][1]]
+        nrow = img2.shape[0]
+        xbins = []
+        for j in range(nrow):
+            count = np.count_nonzero(img2[j, :])
+            xbins.append(count)
+        xslices = get_xslices(xbins)
+        img2 = img2[xslices[0]:xslices[1],:]
+        imgs.append(img2)
+        regions.append([xslices[0],xslices[1],yslices[i][0],yslices[i][1]])
+    return imgs, regions
+
+imgs, regions = get_seg_images(thresh, yslices_new)
+
+# save the segmented images
+# for i in range(len(imgs)):
+#     new_filename = filename.split(".")[0]+"-part-"+str(i+1)+"."+filename.split(".")[1]
+#     cv2.imwrite(new_filename, 255-imgs[i])
 
 
 # test color distance function 1
@@ -134,6 +216,15 @@ for i in range(len(seg)):
 # test color distance 2
 from colormath.color_objects import LabColor, sRGBColor
 from colormath.color_conversions import convert_color
+# convert to hsu
+# hsu = []
+# for y in ybins:
+#     rgb = sRGBColor(y[0],y[1],y[2],True)
+#     print y
+#     lab = convert_color(rgb, LabColor)
+#     hsu.append([lab.lab_l, lab.lab_a, lab.lab_b])
+# plot(hsu)
+
 from colormath.color_diff import delta_e_cie1994
 # ssd_bins = []
 # deltae_bins = []
